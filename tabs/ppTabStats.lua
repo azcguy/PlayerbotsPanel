4f58fe408138cb233364f2190864aad5df7c0de0
azcguy <>
2024-05-27 03:44:04 +0500

    stats tab subtabs

--- tabs/ppTabStats.lua
+++ tabs/ppTabStats.lua
@@ -4,6 +4,7 @@ PlayerbotsPanelTabStats.useFullFrame = false
 PlayerbotsPanelTabStats.useBackground = true
 PlayerbotsPanelTabStats.rightSide = false
 PlayerbotsPanelTabStats.iconTex = PlayerbotsPanelData.ROOT_PATH .. "textures\\icon-tab-stats.tga"
+PlayerbotsPanelTabStats.customSound = "GAMEDIALOGOPEN"
 
 local _tab = nil
 local _frame = nil
@@ -11,6 +12,22 @@ local _frame = nil
 function PlayerbotsPanelTabStats:Init(tab)
     _tab = tab
     _frame = tab.innerframe
+
+    tab:CreateSideButton("Interface\\ICONS\\Spell_Nature_Strength.blp", 
+    function ()
+    end, nil, "Stats")
+
+    tab:CreateSideButton("Interface\\ICONS\\Ability_Repair.blp", 
+        function ()
+        end, nil, "Skills")
+
+    tab:CreateSideButton("Interface\\ICONS\\INV_Misc_Coin_16.blp", 
+        function ()
+        end, nil, "Currencies")
+
+    tab:CreateSideButton("Interface\\ICONS\\Achievement_Reputation_01.blp", 
+        function ()
+        end, nil, "Reputation")
 end
 
 function PlayerbotsPanelTabStats:OnActivate(tab)
